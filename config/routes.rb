1bbe4e14c4030616f4d3a19643791e5792da2f3c
puja-shrestha <shresthaa.puza@gmail.com>
2018-01-25 22:05:23 +0545

    Setting up the static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'ideas#index'
 
   resources :ideas
+
+  get 'about' => 'static_pages#about'
+  get 'random' => 'static_pages#random'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

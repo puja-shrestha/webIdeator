f2b04ca34921f775c967dcb773a80687246f01d6
pawanman56 <pawanman56@gmail.com>
2018-01-27 14:04:35 +0545

    Duplicate migration file was removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180127061456) do
+ActiveRecord::Schema.define(version: 20180127042706) do
 
   create_table "ideas", force: :cascade do |t|
+    t.integer "user_id"
     t.text "description"
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
